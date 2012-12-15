74273f7b9825ede5698a8f0a25900b095e10efb0
Daniel M.C <danielc6503@gmail.com>
2012-12-15 16:12:08 +0000

    Initial Commit

---
+++ Mars/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Mars
+//
+//  Created by Daniel M.C. on 15/12/2012.
+//  Copyright (c) 2012 Daniel M.C. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
